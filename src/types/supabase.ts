c77fda6ee9d6a6e618aeaa51a34aa39b673669de
kaisafestus <kaisafestus11@gmail.com>
2026-04-15 12:00:45 +0300

    fix

--- src/types/supabase.ts
+++ src/types/supabase.ts
@@ -3,3 +3,15 @@ export type Json = string | number | boolean | null | { [key: string]: Json | un
 export interface Database {
   public: {
     Tables: {
+      [key: string]: {
+        Row: { [column: string]: Json }
+        Insert: { [column: string]: Json }
+        Update: { [column: string]: Json }
+      }
+    }
+    Views: { [key: string]: unknown }
+    Functions: { [key: string]: unknown }
+    Enums: { [key: string]: unknown }
+    CompositeTypes: { [key: string]: unknown }
+  }
+}
